9b9bbe97252214b2c94a68d7b8fb1b80f23da31a
sathursan-S6 <sathursansivendran5@gmail.com>
2026-04-25 15:21:21 +0530

    Connect Spring Boot to MySQL

--- src/main/java/com/hotel/reservation/ReservationApplication.java
+++ src/main/java/com/hotel/reservation/ReservationApplication.java
@@ -2,13 +2,8 @@ package com.hotel.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class ReservationApplication {
 
 	public static void main(String[] args) {
